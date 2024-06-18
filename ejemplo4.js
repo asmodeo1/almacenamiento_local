2e629b47645952e39619eb0f025a10fa4d40f7cd
Juan <alonso.jad@gmail.com>
2024-06-18 13:49:06 +0200

    añadido ejercicio 4 (modo claro y oscuro)

---
+++ ejemplo4.js
@@ -0,0 +1,35 @@
+
+function ponerModoClaro() {
+    ponerColor("black", "white");
+
+}
+
+function ponerModoOscuro() {
+    ponerColor("white", "black")
+}
+
+function ponerColor(color, colorFondo) {
+    const texto = document.getElementById("texto");
+    texto.style.backgroundColor = colorFondo;
+    texto.style.color = color;
+    try {
+        // Si no queda espacio se produce una excepción QuotaExceededError
+        localStorage.setItem("color", color);
+        localStorage.setItem("backgroundColor", colorFondo);
+    } catch(excepcion) {
+        window.alert(excepcion);
+    }
+}
+
+function cargarModo() {
+    const color = localStorage.getItem("color");
+    const colorFondo = localStorage.getItem("backgroundColor");
+    if(color != null && colorFondo != null) {
+        ponerColor(color, colorFondo);
+    }
+}
+
+cargarModo();
+document.getElementById("modoClaro").addEventListener("click", ponerModoClaro);
+document.getElementById("modoOscuro").addEventListener("click", ponerModoOscuro);
+
